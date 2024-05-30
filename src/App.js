36863b035beacb4cbd66a81778acab5bebb94378
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-05-30 18:14:43 +0530

    Add mapping function to render state objects with indices

--- src/App.js
+++ src/App.js
@@ -3,14 +3,23 @@ import './App.css';
 import axios from 'axios'
 import {useState} from'react'
 function App() {
+  const [state,setState]=useState([])
   return (
     <div className="App">
     <h1>Hello world</h1> 
     <button onClick={()=>{
       axios.get('https://jsonplaceholder.typicode.com/posts').then((response)=>{
         console.log(response.data)
+        setState(response.data)
       })
     }}>Click Me</button>
+    {state.map((obj,index)=>{
+      return(
+        <div>
+          <h1>{index}</h1>
+        </div>
+      )
+    })}
     </div>
   );
 }
