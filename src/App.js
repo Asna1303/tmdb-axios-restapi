de06a5bf7d56079e786cc2022fbba6be640e4486
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-05-30 10:13:30 +0530

    Add button to fetch posts using Axios from JSONPlaceholder API

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ function App() {
   return (
     <div className="App">
     <h1>Hello world</h1> 
+    <button onClick={()=>{
+      axios.get('https://jsonplaceholder.typicode.com/posts')
+    }}>Click Me</button>
     </div>
   );
 }
